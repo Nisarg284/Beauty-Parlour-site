40f71019cb0a6a7580e056b065f88b49285e8c4b
Nisarg <Nisargsolanki284@gmail.com>
2026-08-14 00:44:18 +0530

    Fix: Remove deprecated studio head.tsx and restore default imageUrlBuilder export

--- lib/sanity.image.ts
+++ lib/sanity.image.ts
@@ -1,7 +1,7 @@
-import { createImageUrlBuilder } from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 import { client } from "./sanity.client";
 
-const builder = createImageUrlBuilder(client);
+const builder = imageUrlBuilder(client);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function urlForImage(source: any) {
